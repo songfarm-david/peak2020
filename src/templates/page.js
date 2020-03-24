e40a16a8a7dd3a64efc7bfcc2ffec7870a3a687f
Dave Gaskin <davidburkegaskin@gmail.com>
2020-03-23 23:23:17 -0700

    commit templates directory

---
+++ src/templates/page.js
@@ -0,0 +1,12 @@
+/**
+ * Page template
+ * Mar 2020
+ */
+
+ import React from "react"
+ import { graphql } from "gatsby"
+ import Layout from "../components/layout"
+
+ export default function PageTemplate({ data }) {
+     console.log(data)
+ }
