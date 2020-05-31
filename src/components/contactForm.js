60c36af9a3ad578b94c4545f5d491091968aeb40
Dave Gaskin <davidburkegaskin@gmail.com>
2020-05-31 00:56:52 -0700

    style contact form and footer

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,23 +1,16 @@
 import React from "react"
 
 import "./contactForm.scss"
+import Form from "./form"
 
 const ContactForm = () => (
     <article id="mainContactForm">
-        <h2>Elevate Your Business</h2>
-        <p>Let us help lead your business to new heights.</p>
-        <form>
-            <label>Name
-                <input type="input" />
-            </label>
-            <label>Email
-                <input type="email" required />
-            </label>
-            <label>How Can We Help?
-                <textarea rows="14"></textarea>
-            </label>
-            <button className="button primary-button">Send</button>
-        </form> 
+        <div>
+            <p className="heading heading-2">Elevate Your Business</p>
+            <p className="sub-heading">Let us help lead your business to new heights.</p>
+            <Form />
+        </div>
+        
     </article>
 )
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -13,59 +13,61 @@ import Newsletter from "./newsletter";
 
 const Footer = ({ footerLinks }) => (
   <footer id="siteFooter">
-    
-    {/* quotes component */}
-    <QuotesCarousel />
-    
-    {/* brand component */}
-    <div className="footer-item">
-      <img src={secondaryLogo} alt={""} />
-      <p className="copyright">© Peak Websites {new Date().getFullYear()}</p>
-    </div>
+    <div>
+      <QuotesCarousel />
+      
+      {/* brand component */}
+      <div className="footer-item">
+        <img src={secondaryLogo} alt={""} />
+        <p className="copyright">© Peak Websites {new Date().getFullYear()}</p>
+      </div>
 
-    {/* secondary nav */}
-    <nav className="footer-item">
-      <ul>
-        {footerLinks.map(link => (
-          <li key={link.name}>
-            <Link to={link.link}>{link.name}</Link>
+      {/* secondary nav */}
+      <nav className="footer-item">
+        <ul>
+          {footerLinks.map(link => (
+            <li key={link.name}>
+              <Link to={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* social component */}
+      <div className="footer-item">
+        <ul>
+          <li>
+              <a href="" title="Peak Websites Facebook Page">
+                <span><img src={facebook} alt={""} /></span>
+                <span>Facebook</span>
+              </a>
           </li>
-        ))}
-      </ul>
-    </nav>
-
-    {/* social component */}
-    <div className="footer-item">
-      <ul>
-        <li>
-            <a href="" title="Peak Websites Facebook Page">
-              <span><img src={facebook} alt={""} /></span>
-              <span>Facebook</span>
+          <li>
+            <a href="" title="Peak Websites Twitter">
+              <span><img src={twitter} alt={""} /></span>
+              <span>Twitter</span>
             </a>
-        </li>
-        <li>
-          <a href="" title="Peak Websites Twitter">
-            <span><img src={twitter} alt={""} /></span>
-            <span>Twitter</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+          </li>
+        </ul>
+      </div>
 
-    {/* Newsletter */}
-    <div className="footer-item">
-      <p>Subscribe to the newsletter</p>
-      <div id="footerNewsletter" className="newsletter">
-        <input type="email" placeholder="Email Address"/>
-        <button className="button small-button footer-text">OK</button>
+      {/* Newsletter */}
+      <div className="footer-item">
+        <p>Subscribe to the newsletter</p>
+        <div id="footerNewsletter" className="newsletter">
+          <input type="email" placeholder="Email Address"/>
+          <button className="button small-button footer-text">OK</button>
+        </div>
+      </div>
+
+      <div className="footer-item">
+        <span>Contact</span>
+        <span><a href="tel:+17785879220">+1 778 587 9220</a></span>
+        <span><a href="mailto:david@peakwebsites.ca">david@peakwebsites.ca</a></span>
       </div>
     </div>
-
-    <div className="footer-item">
-      <span>Contact</span>
-      <span><a href="tel:+17785879220">+1 778 587 9220</a></span>
-      <span><a href="mailto:david@peakwebsites.ca">david@peakwebsites.ca</a></span>
-    </div>
+    
+    
 
   </footer>
 )
