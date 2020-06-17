fddb6a93b09b7482c8eca7ada229fe6388828eec
Dave Gaskin <davidburkegaskin@gmail.com>
2020-06-17 08:53:01 -0700

    create layout class function

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -5,6 +5,7 @@ import Header from "./header"
 import Footer from "./footer"
 
 const Layout = ({ layoutClass, children, path }) => {
+    console.log('Layout component', layoutClass, children, path);
     
     const data = useStaticQuery(graphql`
         query SiteTitleQuery {
@@ -18,10 +19,18 @@ const Layout = ({ layoutClass, children, path }) => {
         }
     `)
   
+    const replaceClass = (cls) => {
+        if (cls.includes('blog-post')) {
+            cls = cls.replace('blog-post', "")
+            return 'blog-post ' + formatTitle(cls)
+        }
+        return formatTitle(cls)
+    }
+
     return ( 
         <>
             <Header siteTitle={data.site.siteMetadata.title} />
-            <main role="main" className={(layoutClass) ? formatTitle(layoutClass) : null}>{children}</main>
+            <main role="main" className={replaceClass(layoutClass)}>{children}</main>
             <Footer path={path} />
         </> 
     )
