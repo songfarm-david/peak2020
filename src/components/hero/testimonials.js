ce923c3d2976cf6b352ce43a9c81c19efe238d51
Dave Gaskin <davidburkegaskin@gmail.com>
2020-06-28 15:05:35 -0700

    add html structure for testimonials

--- src/components/hero/testimonials.js
+++ src/components/hero/testimonials.js
@@ -3,7 +3,7 @@ import React from "react"
 import arrowRight from "../../images/illustrations/svg/Arrows/arrow_right.svg"
 import arrowLeft from "../../images/illustrations/svg/Arrows/arrow_left.svg"
 
-import testiesStyles from "./testimonials.module.scss"
+import "../../styles/theme-styles/testimonials.scss"
 import * as allTestimonials from "../../quotes/testimonials.js"
 
 const Testimonials = () => {
@@ -12,23 +12,23 @@ const Testimonials = () => {
     
     return (
 
-        <section className={testiesStyles.testies}>
+        <section>
             <h2>Testimonials</h2>
-            <div className={testiesStyles.slidersContainer}>
+            <div className="slideContainer">
                 <ul>{testimonials.map((t, idx) => (
-                    <li className={testiesStyles.slide} key={idx} index={idx}>
+                    <li className="slide" key={idx} index={idx} tabIndex="0" >
                         <blockquote>
-                            <span className={`${testiesStyles.heading} heading-3`}>{t.headline}</span>
-                            <span className={testiesStyles.body}>{t.body}</span>
-                            <footer className={testiesStyles.testiFooter}>
-                                <cite className={testiesStyles.cite}>{t.author}</cite>
+                            <span className="heading heading-3">{t.headline}</span>
+                            <span className="body">{t.body}</span>
+                            <footer>
+                                <cite className="cite">{t.author}</cite>
                                 <a href={t.cite}>{t.cite}</a>
                             </footer>
                         </blockquote>
                     </li>))}
                 </ul>
-                <button className={`${testiesStyles.buttons} ${testiesStyles.buttonLeft}`}><img src={arrowLeft} /></button>
-                <button className={`${testiesStyles.buttons} ${testiesStyles.buttonRight}`}><img src={arrowRight} /></button>
+                <button className="buttons buttonLeft"><img src={arrowLeft} /></button>
+                <button className="buttons buttonRight"><img src={arrowRight} /></button>
             </div>
         </section>
             
