d07ca569cb1946bbcc85a3575ae0f3394d822e1e
Dave Gaskin <davidburkegaskin@gmail.com>
2020-07-16 17:40:32 -0700

    update category template page

--- src/templates/categoryTemp.js
+++ src/templates/categoryTemp.js
@@ -15,6 +15,12 @@ export default ({ data, pageContext }) => {
     console.log(data, pageContext);
     const { allWordpressPost } = data 
 
+    const filteredPosts = allWordpressPost.edges.filter(({node}) => {
+        return node.categories.some(({ slug }) => slug === pageContext.slug)
+    })
+
+    console.log(filteredPosts);
+
     return (
         <Layout path={pageContext.url} layoutClass={"category"}>
             <SEO title={pageContext.name} description={(pageContext.description) ? pageContext.description : "Category page for "+pageContext.name} path={pageContext.url} />
@@ -22,9 +28,9 @@ export default ({ data, pageContext }) => {
 
             <PageBanner title={"Category: "+ pageContext.name} slug={"category-"+pageContext.slug} />
             <PageContent path={"category"}>
-                <h2><i>{allWordpressPost.totalCount}</i> Posts in category: {pageContext.name}</h2>
+                <h2><i>{filteredPosts.length}</i> Posts in category: {pageContext.name}</h2>
                 <ul>
-                    {allWordpressPost.edges.map(({ node }, idx ) => (
+                    {filteredPosts.map(({ node }, idx ) => (
                         <li key={idx}><Link to={node.path} title={S(node.title).decodeHTMLEntities().s}>{S(node.title).decodeHTMLEntities().s}</Link></li>
                     ))} 
                 </ul>
@@ -38,14 +44,32 @@ export default ({ data, pageContext }) => {
 
 }
 
+// export const categoryQuery = graphql`
+//     query($slug: String!) {
+//         allWordpressPost(filter: {categories: {elemMatch: {slug: {eq: $slug}}}}) {
+//             edges {
+//                 node {
+//                     id
+//                     title
+//                     path
+//                 }
+//             }
+//             totalCount
+//           }
+//     }
+// `
+
 export const categoryQuery = graphql`
-    query($slug: String!) {
-        allWordpressPost(filter: {categories: {elemMatch: {slug: {eq: $slug}}}}) {
+    query {
+        allWordpressPost {
             edges {
                 node {
                     id
                     title
                     path
+                    categories {
+                        slug
+                    }
                 }
             }
             totalCount
