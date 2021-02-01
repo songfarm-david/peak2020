017a812c69ab392de55859d3014211ca9c45957b
Dave Gaskin <davidburkegaskin@gmail.com>
2021-01-31 19:07:08 -0800

    update

--- src/components/blog/blog-components/blogHeading.js
+++ src/components/blog/blog-components/blogHeading.js
@@ -1,18 +1,17 @@
 import React from "react"
 
-import blogHeadingStyles from "./blogHeading.module.scss"
+import "./blogHeading.scss"
+// import blogHeadingStyles from "./blogHeading.module.scss"
 
 const BlogHeading = ({ headingText, className }) => {
-    // console.log('BlogHeading class?', className);
+    console.log('BlogHeading class?', className);
     
     let callOutClass = (className) ? className : null
 
     return (
-        <div className={( callOutClass ) ? 
-            `${blogHeadingStyles.callOutClass} ${blogHeadingStyles.blogHeading}` : 
-            `${blogHeadingStyles.blogHeading}`}>
-            <h2 className={`${blogHeadingStyles.heading} heading-5`}>{headingText}</h2>
-            <span className={blogHeadingStyles.barline}></span>
+        <div className={`blogHeading ${(callOutClass) ? callOutClass : ''}`}>
+            <h2 className={`heading heading-5`}>{headingText}</h2>
+            <span className={`barline`}></span>
         </div>
     )
 
